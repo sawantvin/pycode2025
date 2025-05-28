2865a0b500a1bcf20e9e397195ed010ff30220e8
sawantvin <vinodm.sawant@gmail.com>
2025-05-28 05:49:23 +0000

    latestUpdate20250528

--- my-repo/scripts/JamesBondMovieAnalysis.py
+++ my-repo/scripts/JamesBondMovieAnalysis.py
@@ -39,7 +39,7 @@ james_bond_data=df.rename(columns=new_columns)
 
 #print(james_bond_data.isna().any(axis="columns"))
 
-print(james_bond_data)
+#print(james_bond_data)
 #print(james_bond_data.select_dtypes(include='number').aggregate([sum,min,max]))
 
 #print(james_bond_data.aggregate({"us_earning": [max,min], "world_earning":[max, min], "budget":[max, min]}))
@@ -49,7 +49,41 @@ print(james_bond_data)
 
 #print(james_bond_data.groupby(['director','bond_actor'])['budget'].transform(lambda x: x.rank(ascending=False)))
 
-james_bond_data.drop(james_bond_data[james_bond_data['year']]==2006)
+#james_bond_data.drop(james_bond_data[james_bond_data['year']]==2006)
+
+data1 = {'Name': ['John', 'Alice', 'Bob', 'Eve'],
+        'Age': [25, 30, 22, 35],
+        'Gender': ['Male', 'Female', 'Male', 'Female']}
+
+df1= pd.DataFrame(data1)
+#print(df1)
+
+data2 = {'Name': ['John', 'Alice', 'Bob', 'Charlie'],
+         'Salary': [50000, 55000, 40000, 48000]}
+
+df2 = pd.DataFrame(data2)
 
 
-#print(james_bond_data)
+
+#print (pd.merge(df1,df2,on='Name',how='inner'))
+
+#df1.set_index('Name', inplace=True)
+#df2.set_index('Name',inplace=True)
+#print (df1)
+#print(df2)
+#print(df1.join(df2))
+
+#print(pd.concat([df1,df2], axis=1))
+print(james_bond_data)
+
+test=pd.DataFrame( { "Name":['Vinod','Advay','Swara'],
+              "Age":[48,30,11]
+              }
+             )
+
+index_=pd.date_range('2025 05 20 08:45', periods=3 , freq='h' )
+
+
+
+
+
